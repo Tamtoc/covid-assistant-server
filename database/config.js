a91ca79bf4b6e045691a14acf62826ce7918024c
Georgy <georgy.1010.011@gmail.com>
2021-05-30 18:17:52 -0500

    config.js updated

--- database/config.js
+++ database/config.js
@@ -6,6 +6,7 @@ const dbConnection = () => {
     const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: process.env.HOST,
         dialect: 'mysql',
+        port:process.env.DB_PORT
         // logging: false,
     });
 
