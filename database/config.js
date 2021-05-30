4a9d122c83711526ff523fe274ecb2885a9248ac
Georgy <georgy.1010.011@gmail.com>
2021-05-30 17:44:08 -0500

    config.js and .env updated

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const dbConnection = () => {
 
 
-    const db = new Sequelize('covid-assistant', 'root', process.env.DB_PASSWORD, {
+    const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
         host: process.env.HOST,
         dialect: 'mysql',
         // logging: false,
