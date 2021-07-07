259cfe6e3d0e89984396309c52749d3cb7eb9796
Georgy <georgy.1010.011@gmail.com>
2021-07-07 18:03:07 -0500

    cors configuration

--- helpers/socketHelper.js
+++ helpers/socketHelper.js
@@ -1,18 +1,13 @@
 const socketHelper = async ( socket, io ) => {
 
     socket.on('connected', (name) => {
-        const welcome = "Bienvenido " + name + "! ¿Que te gustaría conocer?:";
-        const option1 = "1. ¿Cuantas personas han entrado a mi hogar el día de hoy?";
-        const option2 = "1. ¿Cuantas personas han entrado a mi hogar el día de hoy?";
-        const option3 = "3. Gracias";
-        const option4 = "4. Adios";
-        const option5 = "5. Hola";
-        io.emit('messages', { id: null, message: welcome });
-        io.emit('messages', { id: null, message: option1 });
-        io.emit('messages', { id: null, message: option2 });
-        io.emit('messages', { id: null, message: option3 });
-        io.emit('messages', { id: null, message: option4 });
-        io.emit('messages', { id: null, message: option5 });
+        const welcome = "Bienvenido " + name + "! ¿Que te gustaría conocer?: \n" + 
+        "1. ¿Cuantas personas han entrado a mi hogar el día de hoy? \n" +
+        "2. ¿Cuándo fue la última ves que alguien entró a mi casa? \n" +
+        "3. Gracias \n" +
+        "4. Adios \n" +
+        "5. Hola";
+        io.emit('messages', { id: null, message: welcome })
     })
 
     socket.on('message', (id, message) => {
