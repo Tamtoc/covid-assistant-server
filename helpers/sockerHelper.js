8f7fec53f2724f886fe90b010911011626101eb3
Georgy <georgy.1010.011@gmail.com>
2021-07-07 16:58:25 -0500

    socketHelper updated

--- helpers/sockerHelper.js
+++ helpers/sockerHelper.js
@@ -1,6 +1,12 @@
 const socketHelper = async ( socket, io ) => {
 
-    console.log('User connected');
+    socket.on('connected', () => {
+        console.log('User connected');
+    })
+
+    socket.on('message', (id, message) => {
+        io.emit('messages', (id, message));
+    })
 
 }
 
