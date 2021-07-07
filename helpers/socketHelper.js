fcc079b187670cca7c0ee9dc2cd5fa97714f31b6
Georgy <georgy.1010.011@gmail.com>
2021-07-07 17:55:14 -0500

    socketHelper updated

--- helpers/socketHelper.js
+++ helpers/socketHelper.js
@@ -1,12 +1,12 @@
 const socketHelper = async ( socket, io ) => {
 
     socket.on('connected', (name) => {
-        const welcome = `Bienvenido ${name} ! ¿Que te gustaría conocer?:   
-        1. ¿Cuantas personas han entrado a mi hogar el día de hoy? 
-        2. ¿Cuándo fue la última ves que alguien entró a mi casa?
-        3. Gracias
-        4. Adios
-        5. Hola`;
+        const welcome = "Bienvenido " + name + "! ¿Que te gustaría conocer? \n" + 
+        "1. ¿Cuantas personas han entrado a mi hogar el día de hoy?\n" +
+        "2. ¿Cuándo fue la última ves que alguien entró a mi casa?\n" +
+        "3. Gracias\n" +
+        "4. Adios\n" +
+        "5. Hola";
         io.emit('messages', { id: null, message: welcome })
     })
 
