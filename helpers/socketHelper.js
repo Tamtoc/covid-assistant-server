8016a11c5fbdfdf105742174535bf5bf9b90d6cd
Georgy <georgy.1010.011@gmail.com>
2021-07-07 19:57:31 -0500

    socketHelper updated

--- helpers/socketHelper.js
+++ helpers/socketHelper.js
@@ -20,6 +20,9 @@ const socketHelper = async ( socket, io ) => {
             case "¿Cuantas personas han entrado a mi hogar el día de hoy?":
                 const { data, total } = await sensorStore.getData( 10000, 0 );
                 const dates = [];
+                let dd;
+                let mm;
+                let yyyy;
                 let c = 0;
                 data.map((record) => {
                     let fullDate = record.createdAt + '';
@@ -27,9 +30,9 @@ const socketHelper = async ( socket, io ) => {
                     let currentDate = new Date();
                     date = date.split('-');
                     // date = date[2]+ '-' + date[1] + '-' + date[0];
-                    let dd = currentDate.getDate();
-                    let mm = currentDate.getMonth()+1;
-                    let yyyy = currentDate.getFullYear();
+                    dd = currentDate.getDate();
+                    mm = currentDate.getMonth()+1;
+                    yyyy = currentDate.getFullYear();
                     if ( dd == date[2] && mm == date[1] && yyyy == date[0] ) c++;
 
                 });
